c8ca577f229900873c3a037f1fd797203e036991
Nbelman1 <116769408+Nbelman1@users.noreply.github.com>
2026-01-18 21:29:51 -0500

    Added Routes and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,26 @@
 import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css'
 import Header from './components/main-page/Header';
 import Home from './components/main-page/Home';
 import Footer from './components/main-page/Footer';
 import Resume from './components/Resume';
-
+import Contact from './components/Contact';
+import Projects from './components/Projects';
 
 function App() {
 
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <Home />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/contact' element={<Contact />} />
+            <Route path='/projects' element={<Projects />} />
+            <Route path='/resume' element={<Resume />} />
+          </Routes>
       <Footer />
-      <Resume />
-    </>
+    </BrowserRouter>
   );
 };
 
