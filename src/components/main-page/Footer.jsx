d63ffd7f640a85758bd3aaf8df89541199a5f348
Nbelman1 <116769408+Nbelman1@users.noreply.github.com>
2026-01-18 21:32:26 -0500

    Removed extra space after link

--- src/components/main-page/Footer.jsx
+++ src/components/main-page/Footer.jsx
@@ -2,7 +2,7 @@ const Footer = () => {
 
     return (
         <footer>
-            <p> Copyright &copy; <a href="https://github.com/Nbelman1" target="_blank">&nbsp; Noah Belman </a> </p>
+            <p> Copyright &copy; <a href="https://github.com/Nbelman1" target="_blank"> Noah Belman </a> </p>
             {/* TODO: add GitHub icon, LinkedIn icon with links */}
         </footer>
     );
